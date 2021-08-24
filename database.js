8edf5355fecc7b552db644574146e377686ba3d7
magalipujol <magalipuuujol@gmail.com>
2021-08-24 13:07:52 -0300

    changed font size

--- database.js
+++ database.js
@@ -6,8 +6,19 @@ let pages = [
             {
                 link: 'https://docs.google.com/document/d/19EjKcen_5kHtQHTBFqPl341DAK46ChB3eke-zCJN0SE/edit',
                 linkText: 'Cronograma de clases sincrónicas',
-                description: 'none',
+                description: '',
             },
+        ]
+    },
+    // qo2 content
+    {
+        sectionId: 'quimica-organica-2-content',
+        contents: [
+            {
+                link: 'https://comunidades.campusvirtualunr.edu.ar/course/view.php?id=5374',
+                linkText: 'Comunidades',
+                description: '',
+            }
         ]
     }
 
